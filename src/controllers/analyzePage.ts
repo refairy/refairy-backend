1f3cc676934d26a1674312224b1b8025e67bca5f
rycont <reactdev@kakao.com>
2020-11-22 16:06:15 +0900

    fix : save report data to db

--- src/controllers/analyzePage.ts
+++ src/controllers/analyzePage.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import fetch from 'node-fetch'
+import reportModel from '../models/report'
 import getEnv from '../utils/getEnv'
 
 const MODEL_URI = getEnv('MODEL_API')
@@ -8,13 +9,19 @@ const analyzePage = async (req: Request<{
     uri: string
 }>, res: Response) => {
     try {
+        const {uri} = req.params
         const fetched = await (await fetch(MODEL_URI, {
             method: 'POST',
             body: JSON.stringify({
-                uri: req.params.uri
+                uri
             })
         })).json()
-        res.send(fetched)
+        const newReport = new reportModel()
+        newReport.uri = uri
+        newReport.analysisResult = fetched
+        res.send({
+            result: fetched
+        })
     } catch(e) {
         res.status(400).send({
             status: 400,
--- src/models/report.ts
+++ src/models/report.ts
@@ -3,27 +3,30 @@ import {
   createSchema, ExtractDoc, Type, typedModel,
 } from 'ts-mongoose';
 
+const REQUIRED = {
+    required: true
+}
+
 const reportSchema = createSchema({
     uri: Type.string({
-        required: true,
-        trim: true
+        trim: true,
+        ...REQUIRED
     }),
-    analysisResult: Type.array({
-        required: true
-    }).of({
-        
+    analysisResult: Type.array(REQUIRED).of({
+        origin: Type.string(REQUIRED),
+        is_wrong: Type.boolean(REQUIRED),
+        corrected: Type.string(REQUIRED),
+        confidence: Type.number(REQUIRED),
+        catetory: Type.string({
+            enum: ['dokdo'],
+            ...REQUIRED
+        })
     })
 }, {
     timestamps: true,
     _id: true
 })
 
-const reportModel = typedModel('Report', reportSchema, undefined, undefined, {
-    findById(_id: ObjectId) {
-        return this.findOne({
-            _id
-        })
-    }
-})
+const reportModel = typedModel('Report', reportSchema)
 
 export default reportModel
