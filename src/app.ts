d88d96db9e0a44943a2b1627d132276bd404497d
rycont <reactdev@kakao.com>
2020-11-27 10:32:23 +0900

    fix : add cors package

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import express, { json } from 'express';
+import cors from 'cors'
 import index from './router/api';
 
 const app = express();
 app.use(json())
-app.use(cors({ origin: 'https://www.zerocho.com' }))
+app.use(cors({ origin: 'https://refairy.netlify.app' }))
 app.use('/api', index);
 export default app;
