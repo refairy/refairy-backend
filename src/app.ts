2f31f9366ad427237d503313b33bb05ab239fcc9
rycont <reactdev@kakao.com>
2020-11-27 10:32:53 +0900

    feat : add localhost CORS

--- src/app.ts
+++ src/app.ts
@@ -5,5 +5,6 @@ import index from './router/api';
 const app = express();
 app.use(json())
 app.use(cors({ origin: 'https://refairy.netlify.app' }))
+app.use(cors({ origin: 'http://localhost:5500' }))
 app.use('/api', index);
 export default app;
