8947267fb4360bd43a093e67550d9752d8531408
rycont <reactdev@kakao.com>
2020-11-27 21:39:41 +0900

    fix : remove CORS

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import index from './router/api';
 
 const app = express();
 app.use(json())
-app.use(cors({ origin: 'https://refairy.netlify.app' }))
-app.use(cors({ origin: 'http://localhost:5500' }))
+app.use(cors())
 app.use('/api', index);
 export default app;
